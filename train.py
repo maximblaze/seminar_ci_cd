752d0177930d144310981d9ec9cd5cf3a1d019d9
Maxim Doronkin <maximdoronkin@gmail.com>
2025-12-15 17:56:39 +0000

    airflow add

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 import joblib
 
 X, y = load_iris(return_X_y=True)
@@ -7,3 +8,10 @@ model = LogisticRegression(max_iter=200)
 model.fit(X, y)
 joblib.dump(model, "model.pkl")
 print("Model trained successfully.")
+
+X, y = load_iris(return_X_y=True)
+model = LogisticRegression(max_iter=200)
+model.fit(X, y)
+y_pred = model.predict(X)
+y_true = y
+acc = accuracy_score(y_pred, y_true)
